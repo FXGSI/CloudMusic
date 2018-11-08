f6102a205c0fac12604da7e99ee9779cb6a74e9d
unknown <825745273@qq.com>
2018-11-08 09:44:33 +0800

    yunmusic

---
+++ my-music/src/components/singer/singer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="singer" ref="singer">
+    <list-view :data="this.singers" @select="selectSinger" ref="list"></list-view>
+    <router-view></router-view>
+  </div>
+</template>
+
+
+
+<script>
+import Singer from '../../common/js/singer'
+import ListView from '../../base/listview/listview'
+export default {
+    
+
+
+    
+}
+</script>
